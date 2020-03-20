7aaafa9f676ef4b8cd53daf2fcaf329c26ad7d22
Lukas Babaliauskas <babaliauskas@Lukass-MBP.attlocal.net>
2020-03-19 20:13:22 -0400

    added navbar

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
+import { List, Container } from 'semantic-ui-react';
+import { IActivity } from '../models/activity';
+import NavBar from '../../features/nav/NavBar';
+
+const App = () => {
+  const [activities, setActivities] = useState<IActivity[]>([]);
+
+  useEffect(() => {
+    axios
+      .get<IActivity[]>('http://localhost:5000/api/activities')
+      .then(response => setActivities(response.data));
+  }, []);
+
+  return (
+    <Fragment>
+      <NavBar />
+      <Container style={{ paddingTop: '7em' }}>
+        <List>
+          {activities.map(activity => (
+            <List.Item key={activity.id}>{activity.title}</List.Item>
+          ))}
+        </List>
+      </Container>
+    </Fragment>
+  );
+};
+
+export default App;
